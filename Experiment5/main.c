020b2a2c1bdc4695874eb6e9cc00af53fc24fb92
Dinesh Kumar G <gdineshda@gmail.com>
2022-11-23 23:47:35 +0530

    Experiment 5

--- Experiment5/main.c
+++ Experiment5/main.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../Experiment2/link.h"
+
+int main() {
+    int elem, opt;
+    list *stack = init();
+    do {
+        printf("1. Push\n2.Pop\n3.Display\n4.Quit\nEnter your choice: ");
+        scanf("%d", &opt);
+        if (opt == 1) {
+            printf("Enter the elem: ");
+            scanf("%d", &elem);
+            push(stack, elem);
+        } else if (opt == 2) {
+            elem = pop(stack);
+            printf("Popped: %d\n", elem);
+        } else if (opt == 3) {
+            traverse(stack);
+        }
+    } while (opt != 4);
+    printf("Bye\n");
+    return 0;
+}
