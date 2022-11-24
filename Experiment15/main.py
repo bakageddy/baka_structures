fac9d5c6cdf3ab093f2eb3022df8f7c56d6ff3ef
Dinesh Kumar G <gdineshda@gmail.com>
2022-11-24 20:33:32 +0530

    Experiment 15

---
+++ Experiment15/main.py
@@ -0,0 +1,14 @@
+def insertion(list):
+    for i in range(1, len(list)):
+        key = list[i]
+        j = i - 1
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
+            j -= 1
+        list[j + 1] = key
+    return list
+
+
+hehe = list(map(int, input().strip().split()))
+print(hehe)
+print(insertion(hehe))
